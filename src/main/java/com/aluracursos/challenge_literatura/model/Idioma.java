5ec6fb052fc722bcffc21efa83d42ae5730b2832
Arturombl <arturomobl@gmail.com>
2024-07-13 23:04:42 -0700

    Correccion de Idiomas

--- src/main/java/com/aluracursos/challenge_literatura/model/Idioma.java
+++ src/main/java/com/aluracursos/challenge_literatura/model/Idioma.java
@@ -1,27 +1,39 @@
 package com.aluracursos.challenge_literatura.model;
 
 public enum Idioma {
-        ES("es"),
-        FR("fr"),
-        EN("en"),
-        PT("pt");
+    ES("es", "Español"),
+    FR("fr" , "Frances"),
+    EN("en", "Ingles"),
+    PT("pt", "Protuges"),
+    JP("jp", "Japones");
 
-        private String idioma;
+    private String idioma;
+    private String nombreIdiomaCompleto;
 
-        Idioma(String idioma) {
-            this.idioma = idioma;
-        }
+    Idioma(String idioma, String nombreIdiomaCompleto) {
+        this.idioma = idioma;
+        this.nombreIdiomaCompleto = nombreIdiomaCompleto;
+    }
 
-        public String getIdioma(){
-            return this.idioma;
-        }
+    public String getIdioma(){
+        return this.idioma;
+    }
 
-        public static Idioma fromString(String text) {
-            for (Idioma idioma : Idioma.values()) {
-                if (idioma.idioma.equalsIgnoreCase(text)) {
-                    return idioma;
-                }
+    public static Idioma fromString(String text) {
+        for (Idioma idioma : Idioma.values()) {
+            if (idioma.idioma.equalsIgnoreCase(text)) {
+                return idioma;
             }
-            throw new IllegalArgumentException("No constant with text " + text + " found");
         }
+        throw new IllegalArgumentException("No constant with text " + text + " found");
+    }
+
+    public static Idioma fromCompleto(String text) {
+        for (Idioma idioma : Idioma.values()) {
+            if (idioma.nombreIdiomaCompleto.equalsIgnoreCase(text)) {
+                return idioma;
+            }
+        }
+        throw new IllegalArgumentException("No constant with text " + text + " found");
+    }
 }
